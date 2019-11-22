2d93b78f7ed18973eb80af42efc47aafdbb815e2
William Capozzoli <thinkocapo@gmail.com>
2019-11-22 15:55:20 -0800

    many fixes by manoel

--- app/src/main/java/com/example/vu/android/MainActivity.java
+++ app/src/main/java/com/example/vu/android/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.vu.android;
 
 import android.os.Bundle;
-import io.sentry.android.AndroidSentryClientFactory;
-import android.support.v7.app.AppCompatActivity;
+// are no longer relevant with io.sentry:sentry-android:2.0.+ ?
+//import io.sentry.android.AndroidSentryClientFactory;
+//import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
-import io.sentry.Sentry;
-import io.sentry.event.Breadcrumb;
-import io.sentry.event.BreadcrumbBuilder;
-import io.sentry.event.UserBuilder;
+//import io.sentry.Sentry;
+//import io.sentry.event.Breadcrumb;
+//import io.sentry.event.BreadcrumbBuilder;
+//import io.sentry.event.UserBuilder;
 import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     TextView total;
@@ -24,38 +27,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //  Sentry DSN + configuration defined in app/src/main/resources/sentry.properties file
-        Sentry.init(new AndroidSentryClientFactory(this));
+//        Sentry.init(new AndroidSentryClientFactory(this));
 
 
-        Button submit_email_button = (Button)findViewById(R.id.submit_email);
-        submit_email_button.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-
-                InputMethodManager input = (InputMethodManager)
-                        getSystemService(Context.INPUT_METHOD_SERVICE);
-                input.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
-
-                EditText email = (EditText) findViewById(R.id.email);
-                TextView display = (TextView) findViewById(R.id.display);
-                String email_string = email.getText().toString();
-                Sentry.getContext().setUser(
-                  new UserBuilder().setEmail(email_string).build()
-                );
-                String greeting = "Hello, " + email_string + "!";
-                display.setText(greeting);
-
-
-            }
-        });
+//        Button submit_email_button = (Button)findViewById(R.id.submit_email);
+//        submit_email_button.setOnClickListener( new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view){
+//
+//                InputMethodManager input = (InputMethodManager)
+//                        getSystemService(Context.INPUT_METHOD_SERVICE);
+//                input.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+//
+//                EditText email = (EditText) findViewById(R.id.email);
+//                TextView display = (TextView) findViewById(R.id.display);
+//                String email_string = email.getText().toString();
+////                Sentry.getContext().setUser(
+////                  new UserBuilder().setEmail(email_string).build()
+////                );
+//                String greeting = "Hello, " + email_string + "!";
+//                display.setText(greeting);
+//
+//
+//            }
+//        });
 
         final Button anr_button = (Button)findViewById(R.id.anr_button);
         anr_button.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                Sentry.getContext().recordBreadcrumb(
-                        new BreadcrumbBuilder().setLevel(Breadcrumb.Level.DEBUG).setCategory("custom").setType(Breadcrumb.Type.USER).setMessage("User clicked button: ANR").build()
-                );
+//                Sentry.getContext().recordBreadcrumb(
+//                        new BreadcrumbBuilder().setLevel(Breadcrumb.Level.DEBUG).setCategory("custom").setType(Breadcrumb.Type.USER).setMessage("User clicked button: ANR").build()
+//                );
 
                 while(true) {
                     //Wait 5 seconds for ANR....
@@ -68,9 +71,9 @@ public class MainActivity extends AppCompatActivity {
         div_by_zero_button.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                Sentry.getContext().recordBreadcrumb(
-                    new BreadcrumbBuilder().setLevel(Breadcrumb.Level.DEBUG).setCategory("custom").setType(Breadcrumb.Type.USER).setMessage("User clicked button: DIVIDE BY ZERO").build()
-                );
+//                Sentry.getContext().recordBreadcrumb(
+//                    new BreadcrumbBuilder().setLevel(Breadcrumb.Level.DEBUG).setCategory("custom").setType(Breadcrumb.Type.USER).setMessage("User clicked button: DIVIDE BY ZERO").build()
+//                );
                 int t = 5 / 0 ;
             }
         });
@@ -79,9 +82,9 @@ public class MainActivity extends AppCompatActivity {
         negative_index_button.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                Sentry.getContext().recordBreadcrumb(
-                    new BreadcrumbBuilder().setLevel(Breadcrumb.Level.DEBUG).setCategory("custom").setType(Breadcrumb.Type.USER).setMessage("User clicked button: NEGATIVE INDEX").build()
-                );
+//                Sentry.getContext().recordBreadcrumb(
+//                    new BreadcrumbBuilder().setLevel(Breadcrumb.Level.DEBUG).setCategory("custom").setType(Breadcrumb.Type.USER).setMessage("User clicked button: NEGATIVE INDEX").build()
+//                );
                 int[] a = new int[-5];
             }
         });
@@ -91,15 +94,15 @@ public class MainActivity extends AppCompatActivity {
         file_not_found_button.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                Sentry.getContext().recordBreadcrumb(
-                    new BreadcrumbBuilder().setLevel(Breadcrumb.Level.DEBUG).setCategory("custom").setType(Breadcrumb.Type.USER).setMessage("User clicked button: FILE NOT FOUND").build()
-                );
-
-                try {
-                    Integer.parseInt ("str");
-                } catch (Exception e) {
-                    Sentry.capture(e);
-                }
+//                Sentry.getContext().recordBreadcrumb(
+//                    new BreadcrumbBuilder().setLevel(Breadcrumb.Level.DEBUG).setCategory("custom").setType(Breadcrumb.Type.USER).setMessage("User clicked button: FILE NOT FOUND").build()
+//                );
+//
+//                try {
+//                    Integer.parseInt ("str");
+//                } catch (Exception e) {
+//                    Sentry.capture(e);
+//                }
 
             }
         });
