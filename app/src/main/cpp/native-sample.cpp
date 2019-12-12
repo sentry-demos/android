f169bb96323b1e7168cf3485981add0f0ca42dd6
William Capozzoli <thinkocapo@gmail.com>
2019-12-12 13:11:55 -0800

    sentry-nativesample

--- app/src/main/cpp/native-sample.cpp
+++ app/src/main/cpp/native-sample.cpp
@@ -1,15 +1,19 @@
-
 #include <jni.h>
 #include <android/log.h>
+#include <sentry.h>
 
-#define TAG "sentry-android-sample"
+#define TAG "sentry-nativesample"
 
 extern "C" {
 
-JNIEXPORT void JNICALL Java_io_sentry_sample_NativeSample_crash(JNIEnv *env, jclass cls) {
+JNIEXPORT void JNICALL Java_io_sentry_nativesample_NativeSample_message(JNIEnv *env, jclass cls) {
     __android_log_print(ANDROID_LOG_WARN, TAG, "About to crash.");
-    char *ptr = 0;
-    *ptr += 1;
+    sentry_value_t event = sentry_value_new_message_event(
+      /*   level */ SENTRY_LEVEL_INFO,
+      /*  logger */ "custom",
+      /* message */ "It works!"
+    );
+    sentry_capture_event(event);
 }
 
 }
