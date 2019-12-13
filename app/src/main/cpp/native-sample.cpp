ea67ec6b691b23bef601da415a8bd566723376d2
William Capozzoli <thinkocapo@gmail.com>
2019-12-12 17:06:59 -0800

    Native Crash works

--- app/src/main/cpp/native-sample.cpp
+++ app/src/main/cpp/native-sample.cpp
@@ -6,7 +6,7 @@
 
 extern "C" {
 
-JNIEXPORT void JNICALL Java_io_sentry_nativesample_NativeSample_message(JNIEnv *env, jclass cls) {
+JNIEXPORT void JNICALL Java_com_example_vu_android_NativeSample_message(JNIEnv *env, jclass cls) {
     __android_log_print(ANDROID_LOG_WARN, TAG, "About to crash.");
     sentry_value_t event = sentry_value_new_message_event(
       /*   level */ SENTRY_LEVEL_INFO,
