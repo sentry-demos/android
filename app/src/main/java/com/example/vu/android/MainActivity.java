52f4ab27a61225ad2f2b7fe39ad7dbe4f9c7a031
William Capozzoli <thinkocapo@gmail.com>
2020-01-02 09:49:11 -0500

    shortened Breadcrumbs

--- app/src/main/java/com/example/vu/android/MainActivity.java
+++ app/src/main/java/com/example/vu/android/MainActivity.java
@@ -32,26 +32,26 @@ public class MainActivity extends AppCompatActivity {
         // SENTRY Tag and Breadcrumb
         String activity = this.getClass().getSimpleName();
         Sentry.setTag("activity", activity);
-        Sentry.addBreadcrumb(new Breadcrumb(activity + " was created"));
+        Sentry.addBreadcrumb(activity + " was created");
 
         // DIVIDE BY ZERO - ArithmeticException
         Button div_by_zero_button = findViewById(R.id.div_zero);
         div_by_zero_button.setOnClickListener(view -> {
-            Sentry.addBreadcrumb(new Breadcrumb("Button for Error 1 clicked..."));
+            Sentry.addBreadcrumb("Button for Error 1 clicked...");
             int t = 5 / 0;
         });
 
         // NEGATIVE INDEX - NegativeArraySizeException
         Button negative_index_button = findViewById(R.id.negative_index);
         negative_index_button.setOnClickListener(view -> {
-            Sentry.addBreadcrumb(new Breadcrumb("Button for Error 2 clicked..."));
+            Sentry.addBreadcrumb("Button for Error 2 clicked...");
             int[] a = new int[-5];
         });
 
         // HANDLED EXCEPTION - NumberFormatException
         Button handled_exception_button = findViewById(R.id.handled_exception);
         handled_exception_button.setOnClickListener(view -> {
-                Sentry.addBreadcrumb(new Breadcrumb("Button for Error 3 (Handled Exception) clicked..."));
+                Sentry.addBreadcrumb("Button for Error 3 (Handled Exception) clicked...");
                 try {
                     Integer.parseInt ("str");
                 } catch (Exception e) {
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         // APPLICATION NOT RESPONDING
         Button anr_button = findViewById(R.id.anr);
         anr_button.setOnClickListener(view -> {
-            Sentry.addBreadcrumb(new Breadcrumb("Button for ANR clicked..."));
+            Sentry.addBreadcrumb("Button for ANR clicked...");
             while(true) {
                 // Wait 2 seconds for ANR....
             }
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
         // NATIVE CRASH
         findViewById(R.id.native_crash).setOnClickListener(view -> {
-            Sentry.addBreadcrumb(new Breadcrumb("Button for Native Crash clicked..."));
+            Sentry.addBreadcrumb("Button for Native Crash clicked...");
             NativeSample.crash();
         });
 
