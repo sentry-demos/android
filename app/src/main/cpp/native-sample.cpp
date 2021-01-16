4fc515ea55847cc80d05f7d9c3cc4c4e6ec0c594
Ido Shemesh <ishemesh@sentry.io>
2021-01-15 18:14:53 -0800

    reset changes to file

--- app/src/main/cpp/native-sample.cpp
+++ app/src/main/cpp/native-sample.cpp
@@ -11,15 +11,6 @@ extern "C" {
 JNIEXPORT void JNICALL Java_com_example_vu_android_NativeSample_crash(JNIEnv *env, jclass cls) {
     __android_log_print(ANDROID_LOG_WARN, TAG, "About to crash.");
 
-//    sentry_value_t user = sentry_value_new_object();
-//    sentry_value_set_by_key(user, "id", sentry_value_new_int32(42));
-//    sentry_value_set_by_key(user, "email", sentry_value_new_string("john.doe@example.org"));
-//    sentry_value_set_by_key(user, "username", sentry_value_new_string("John Doe"));
-//    sentry_set_user(user);
-
-    sentry_set_tag("transaction_id", "lkdfjs4001");
-
-
     char *ptr = 0;
     *ptr += 1;
 }
