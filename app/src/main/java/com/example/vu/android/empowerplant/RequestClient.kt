c21f361ccec0c8e81dd2468712ec64ae987a6e41
Simon Zhong <simon.zhong@sentry.io>
2023-07-12 22:06:44 -0700

    add SentryOkHttpEventListener

--- app/src/main/java/com/example/vu/android/empowerplant/RequestClient.kt
+++ app/src/main/java/com/example/vu/android/empowerplant/RequestClient.kt
@@ -1,6 +1,7 @@
 package com.example.vu.android.empowerplant
 
 import io.sentry.android.okhttp.SentryOkHttpInterceptor
+import io.sentry.android.okhttp.SentryOkHttpEventListener
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
 import io.sentry.HttpStatusCodeRange
@@ -12,6 +13,7 @@ public class RequestClient {
                     captureFailedRequests = true,
                     failedRequestStatusCodes = listOf(HttpStatusCodeRange(400, 599))
             ))
+            .eventListener(SentryOkHttpEventListener())
             .connectTimeout(10, TimeUnit.SECONDS)
             .writeTimeout(10, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
