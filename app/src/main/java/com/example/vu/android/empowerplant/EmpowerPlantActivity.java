06307f50aacf27e9fac8874588c2094a8052b4d0
stefanosiano <stefano.siano91@gmail.com>
2023-12-05 10:15:05 +0100

    reduced slow query time

--- app/src/main/java/com/example/vu/android/empowerplant/EmpowerPlantActivity.java
+++ app/src/main/java/com/example/vu/android/empowerplant/EmpowerPlantActivity.java
@@ -43,7 +43,7 @@ public class EmpowerPlantActivity extends MyBaseActivity {
                 .StoreItemDAO().deleteAll();
 
         List<StoreItem> tmpStoreItems = new ArrayList<StoreItem>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
                 StoreItem storeitem = new StoreItem();
                 storeitem.setName(genRandomString());
                 storeitem.setSku(genRandomString());
