c6deb3fece9d497595dadcd9e581686b8b35841b
stefanosiano <stefano.siano91@gmail.com>
2024-04-04 12:39:29 +0200

    added custom ContentProvider to increment app start duration enabled perfV2 and app start profiling

--- app/src/main/java/com/example/vu/android/MyApplication.java
+++ app/src/main/java/com/example/vu/android/MyApplication.java
@@ -70,6 +70,8 @@ public class MyApplication extends Application {
             );
 
             options.setAttachThreads(true);
+            options.setEnableAppStartProfiling(true);
+            options.setEnablePerformanceV2(true);
             options.setBeforeSend((event, hint) -> {
 
                 //Remove PII
