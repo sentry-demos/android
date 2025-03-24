818da02a37bcc92600bd3f20f27d8ab998f14193
Kosty Maleyev <kosty.maleyev@sentry.io>
2025-03-24 14:07:50 -0700

    Pretty sure we don't need this variable... (#168)

--- app/src/main/java/com/example/vu/android/empowerplant/MainFragment.java
+++ app/src/main/java/com/example/vu/android/empowerplant/MainFragment.java
@@ -415,7 +415,7 @@ public class MainFragment extends Fragment implements StoreItemAdapter.ItemClick
         ISpan processDeliverySpan = checkoutTransaction.startChild("task", "process delivery");
 
         try {
-            throw new MainFragment.BackendAPIException("Failed to init delivery item workflow");
+            throw new MainFragment.BackendAPIException("Failed to init delivery workflow");
         } catch (Exception e) {
             Log.e("processDeliveryItem", e.getMessage());
             processDeliverySpan.setThrowable(e);
